a686b231e01ae6fedebf7c942f87e81972d646a9
mrugankray <mrugankray2@gmail.com>
2018-07-13 14:03:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Natural_Language_Processing
+# Written in python.
+#Create a new Project/script.
+#Copy the script that i have provided to the script that you have created in the above step and it's named as nlp.py.
+#Restaurant_Reviews.tsv file is there,so just copy/cut it to your working directory.
+#Now you are good to go
+#Good Luck :)
---
+++ nlp.py
@@ -0,0 +1,47 @@
+# importing all libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv',quoting = 3, delimiter = '\t')
+
+# cleaning the texts
+import re 
+import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(words) for words in review if not words in  set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+# building bag of words
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+
+#splitting the dataset
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+#fitting the calissifier into the training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train,y_train)
+
+#predicting test set results
+y_pred = classifier.predict(X_test)
+ 
+#creating the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+#accuracy
+accuracy = ((55+91)/200)*100
